dfab41ccc7702817b91d21f0f578f86f15880b09
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2024-12-17 01:00:24 +0530

    fist commit

---
+++ src/test/java/org/eazybytes/eazyschool/EazybytesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eazybytes.eazyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazybytesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
